b35ec1c78e61f6fba3e5f2d42025e98360692796
sofia Luna <Sofia21luna@gmail.com>
2020-09-04 11:13:14 -0400

    header component is completed

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,34 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+//parent container
+const headerParent = document.querySelector('.header-container');
+
+function Header() {
+
+    //create elements
+    const headerComp = document.createElement("div");
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    //add content to elements
+    date.textContent = 'March 28, 2020';
+    title.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    //add classes
+    headerComp.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp')
+
+    //append children to parent
+    headerComp.appendChild(date);
+    headerComp.appendChild(title);
+    headerComp.appendChild(temp);
+
+    //return parent component
+    return headerComp;
+}
+
+headerParent.appendChild(Header());
